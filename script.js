e43e4b99e5d731fb34f0087b34152e357c12d509
Parth <parthwani22@gmail.com>
2025-11-22 22:06:44 +0530

    Added comments in js

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
+// Get textArea where the user has entered the markdown text.
 const textArea = document.getElementById('inputText');
+// Focus on text area as soon as page is loaded.
 textArea.focus();
+// Function renders the markdown text.
 function renderText() {
+    // Get the value of the input text.
     const inputText = textArea.value;
-
+    // Split on new line and create lines array.
     const linesArray = inputText.split('\n');
-
+    // Create converted markdown array
     const mdArray = linesArray.map((line, idx) => {
+        // Check sequence - Headings, Bold, Italic, Links
         return checkLinks(checkItalic(checkBold(checkHeadings(line))));
     });
 
+    // Get the space where the text is to be rendered
     const renderText = document.getElementById('renderedText');
+    // Join the markdown array on new line.
     renderText.innerHTML = mdArray.join('\n');
 };
 
+// Function to replace links with <a> tag using regex.
 function checkLinks(line) {
     return line.replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2">$1</a>');
 }
 
+// Function to replace md bold with <strong> tag using regex.
 function checkBold(line) {
     return line.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>');
 };
 
+// Function to replace md italix with <em> tag using regex.
 function checkItalic(line) {
     return line.replace(/\*(.*?)\*/g, '<em>$1</em>');
 };
 
+// Function to check heading start and assigning appropriate headings.
 function checkHeadings(line) {
     let outputText = "";
     if(line.startsWith('###')) {
@@ -39,6 +50,7 @@ function checkHeadings(line) {
     return outputText;
 }
 
+// Function to reset text area value.
 function resetTextArea() {
     textArea.value = "";
     textArea.focus();
