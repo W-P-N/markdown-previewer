0f8c2fb8eddfb0a028df5129993836b3f38c5807
Parth <parthwani22@gmail.com>
2025-11-22 22:36:28 +0530

    Remove direct call from html. Use IIFE instead of keeping everything in global scope.

--- script.js
+++ script.js
@@ -1,58 +1,68 @@
-// Get textArea where the user has entered the markdown text.
-const textArea = document.getElementById('inputText');
-// Focus on text area as soon as page is loaded.
-textArea.focus();
-// Function renders the markdown text.
-function renderText() {
-    // Get the value of the input text.
-    const inputText = textArea.value;
-    // Split on new line and create lines array.
-    const linesArray = inputText.split('\n');
-    // Create converted markdown array
-    const mdArray = linesArray.map((line, idx) => {
-        // Check sequence - Headings, Bold, Italic, Links
-        return checkLinks(checkItalic(checkBold(checkHeadings(line))));
-    });
+(
+    function (){
+        const convertButton = document.getElementById('convertBtn');
+        const resetButton = document.getElementById('resetBtn');
 
-    // Get the space where the text is to be rendered
-    const renderText = document.getElementById('renderedText');
-    // Join the markdown array on new line.
-    renderText.innerHTML = mdArray.join('\n');
-};
+        convertButton.addEventListener('onclick', renderText);
+        resetButton.addEventListener('onclick', resetTextArea);
 
-// Function to replace links with <a> tag using regex.
-function checkLinks(line) {
-    return line.replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2">$1</a>');
-}
+        // Focus on text area as soon as page is loaded.
+        textArea.focus();
 
-// Function to replace md bold with <strong> tag using regex.
-function checkBold(line) {
-    return line.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>');
-};
+        // Function renders the markdown text.
+        function renderText() {
+            // Get the text area
+            const textArea = document.getElementById('inputText');
+            // Get the value of the input text.
+            const inputText = textArea.value;
+            // Split on new line and create lines array.
+            const linesArray = inputText.split('\n');
+            // Create converted markdown array
+            const mdArray = linesArray.map((line, idx) => {
+                // Check sequence - Headings, Bold, Italic, Links
+                return checkLinks(checkItalic(checkBold(checkHeadings(line))));
+            });
 
-// Function to replace md italix with <em> tag using regex.
-function checkItalic(line) {
-    return line.replace(/\*(.*?)\*/g, '<em>$1</em>');
-};
+            // Get the space where the text is to be rendered
+            const renderText = document.getElementById('renderedText');
+            // Join the markdown array on new line.
+            renderText.innerHTML = mdArray.join('\n');
+        };
 
-// Function to check heading start and assigning appropriate headings.
-function checkHeadings(line) {
-    let outputText = "";
-    if(line.startsWith('###')) {
-        outputText += `<h3>${line.slice(3)}</h3>\n`;
-    } else if(line.startsWith('##')) {
-        outputText += `<h2>${line.slice(2)}</h2>\n`;
-    } else if(line.startsWith('#')) {
-        outputText += `<h1>${line.slice(1)}</h1>\n`;
-    } else {
-        outputText += `<p>${line}</p>\n`;
-    };
-    return outputText;
-}
+        // Function to replace links with <a> tag using regex.
+        function checkLinks(line) {
+            return line.replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2">$1</a>');
+        }
 
-// Function to reset text area value.
-function resetTextArea() {
-    textArea.value = "";
-    textArea.focus();
-};
+        // Function to replace md bold with <strong> tag using regex.
+        function checkBold(line) {
+            return line.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>');
+        };
 
+        // Function to replace md italix with <em> tag using regex.
+        function checkItalic(line) {
+            return line.replace(/\*(.*?)\*/g, '<em>$1</em>');
+        };
+
+        // Function to check heading start and assigning appropriate headings.
+        function checkHeadings(line) {
+            let outputText = "";
+            if(line.startsWith('###')) {
+                outputText += `<h3>${line.slice(3)}</h3>\n`;
+            } else if(line.startsWith('##')) {
+                outputText += `<h2>${line.slice(2)}</h2>\n`;
+            } else if(line.startsWith('#')) {
+                outputText += `<h1>${line.slice(1)}</h1>\n`;
+            } else {
+                outputText += `<p>${line}</p>\n`;
+            };
+            return outputText;
+        }
+
+        // Function to reset text area value.
+        function resetTextArea() {
+            textArea.value = "";
+            textArea.focus();
+        };
+
+})();
